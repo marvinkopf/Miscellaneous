2ccf0105d5ec2c351b09a4f0babe3c271fa7ab5c
3rr0rl0l <marvinkopf@posteo.de>
2015-12-25 08:15:32 +0100

    add 99 bottles of beer

--- RosettaCode/99BottlesOfBeer/Program.cs
+++ RosettaCode/99BottlesOfBeer/Program.cs
@@ -6,7 +6,16 @@ namespace BottlesOfBeer
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("***99 Bottles of Beer***");
+
+			for(int i = 99; i > 0; i--) {
+				Console.Write("\n{0} bottles of beer on the wall \n" +
+					"{0} bottles of beer \nTake one down, pass it around \n" +
+					"{1} bottles of beer on the wall\n", i, i-1);
+			}
+
+			Console.WriteLine("\nNo more bottles of beer on the wall, no more bottles of beer.\n" +
+				"Go to the store and buy some more, 99 bottles of beer on the wall.");
 		}
 	}
 }
