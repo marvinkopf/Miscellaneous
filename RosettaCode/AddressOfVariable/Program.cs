e339be848f604af8ca230b119d2ed32fa2a65a01
3rr0rl0l <marvinkopf@posteo.de>
2015-12-25 21:14:41 +0100

    add Get Adress of Variable

---
+++ RosettaCode/AddressOfVariable/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressOfVariable
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{	
+			unsafe {
+				int i;
+				Console.WriteLine ((int)&i);
+			}
+		}
+	}
+}
