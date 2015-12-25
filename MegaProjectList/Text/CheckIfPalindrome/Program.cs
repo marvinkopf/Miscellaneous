57190de722a38dc06a24d7ef89d5c1dd7b387cb9
3rr0rl0l <marvinkopf@posteo.de>
2015-12-25 07:09:09 +0100

    add Check if Palindrome

---
+++ MegaProjectList/Text/CheckIfPalindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CheckIfPalindrome
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("***Check if Palindrome****\n-------------\n");
+
+			string input = Console.ReadLine();
+			string s = "";
+
+			for(int i = 0; i < input.Length; i++)
+				s = input[i] + s;
+		
+
+			if ( input == s)
+				Console.Write(true);
+			else
+				Console.Write(false);
+
+		}
+	}
+}
