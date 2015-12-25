aa4019cf71edc04dc28f6c3b04ad6614b9e23a09
3rr0rl0l <marvinkopf@posteo.de>
2015-12-25 08:08:22 +0100

    add 100doors

---
+++ RosettaCode/100Doors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Doors
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("***100 DOORS***\n--------\n");
+
+			bool[] doors = new bool[100];
+
+			for (int i = 1; i <= 100; i++) 
+				for (int j = i - 1; j < 100; j += i) 
+					doors [j] = !doors [j];
+				
+			for (int i = 0; i < 100; i++)
+				Console.Write ("\nDoor #{0} is: {1}", i+1, doors [i] ? "Open" : "Closed");
+
+		}
+	}
+}
