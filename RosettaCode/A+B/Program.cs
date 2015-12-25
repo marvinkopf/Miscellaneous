e1e0482354ab744d270ed05046153c6fd2eeab78
3rr0rl0l <marvinkopf@posteo.de>
2015-12-25 08:20:53 +0100

    add A+B

---
+++ RosettaCode/A+B/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AB
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("****A+B****\n-----");
+
+			string[] s = Console.ReadLine().Split();
+
+			Console.Write(int.Parse(s[0]) + int.Parse(s[1]));
+		}
+	}
+}
