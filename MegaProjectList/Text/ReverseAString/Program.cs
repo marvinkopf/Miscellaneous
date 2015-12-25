9068343f468b1a039569fb781ac00343fc320723
3rr0rl0l <marvinkopf@posteo.de>
2015-12-25 05:47:33 +0100

    add reverse a string

---
+++ MegaProjectList/Text/ReverseAString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseAString
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("***REVERSE A STRING***\n\n Input string:");
+
+			string input = Console.ReadLine(); 
+			string result = "";
+
+			for(int i = input.Length -1; i >= 0; i--) {
+				result += input[i];
+			}
+
+			Console.WriteLine(result);
+		}
+	}
+}
