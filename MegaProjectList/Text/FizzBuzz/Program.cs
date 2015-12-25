4ba8b4be973f75d18528c1361459619ac4b3736f
3rr0rl0l <marvinkopf@posteo.de>
2015-12-25 05:44:08 +0100

    add fizzbuzz

---
+++ MegaProjectList/Text/FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("***FIZZ BUZZ***\n------------------\n");
+
+			for (int i = 1; i <= 100; i++) {
+				if (i % 3 == 0) {
+					Console.Write ("\nFizz");
+					if (i % 5 == 0)
+						Console.Write ("Buzz");
+				} else if (i % 5 == 0)
+					Console.Write ("\nBuzz");
+				else
+					Console.Write ("\n" + i);
+			}
+
+		}
+	}
+}
