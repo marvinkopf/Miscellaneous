c87b01a630cca382c1858e5891ed338f3ef40db1
3rr0rl0l <marvinkopf@posteo.de>
2015-12-27 03:16:10 +0100

    add biggest prime factor

---
+++ LargestPrimeFactor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LargestPrimeFactor
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			ulong n = 600851475143;
+			ulong max = 0;
+
+			for(ulong i = 1; i*i <= n ;i++) {
+				if(n % i != 0)
+					continue;
+
+				ulong count = 0;
+				for(ulong j = 1; j <= i; j++) {
+					if(i%j==0) {
+						count++;
+						if(count > 2)
+							break; 
+					}
+				}
+				if (count == 2) {
+					max = i;
+				}
+			}
+			Console.WriteLine (max);
+		}
+	}
+}
