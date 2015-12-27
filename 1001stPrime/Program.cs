497300311c23f7fe69588b4b6e65302c30994841
3rr0rl0l <marvinkopf@posteo.de>
2015-12-27 03:52:49 +0100

    add 1001 prime number

---
+++ 1001stPrime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace stPrime
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int counterOfPrimeNumber = 0;
+			for(ulong i = 1; i*i <= ulong.MaxValue ;i++) {
+				ulong count = 0;
+				for(ulong j = 1; j <= i; j++) {
+					if(i%j==0) {
+						count++;
+						if(count > 2)
+							break; 
+					}
+				}
+				if (count == 2) {
+					counterOfPrimeNumber++;
+					if(counterOfPrimeNumber == 10001)
+						Console.WriteLine(i);
+				}
+			}
+
+		}
+	}
+}
