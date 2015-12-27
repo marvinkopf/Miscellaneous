36e165e5ee2c2b633f5b613631afbf00fb0add7a
3rr0rl0l <marvinkopf@posteo.de>
2015-12-27 03:48:54 +0100

    add Sum Square Difference

---
+++ SumSquareDifference/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumSquareDifference
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int sumSquares = 0;
+			int sumNumbers = 0;
+
+			for(int i = 1; i <= 100; i++) {
+				sumSquares += i*i;
+				sumNumbers += i;
+			}
+
+			sumNumbers *= sumNumbers;
+
+			Console.WriteLine(sumNumbers - sumSquares);
+		}
+	}
+}
