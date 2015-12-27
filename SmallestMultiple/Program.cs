2710d3a585071bb4f952aaa837c445406c4dda2f
3rr0rl0l <marvinkopf@posteo.de>
2015-12-27 03:40:14 +0100

    add smallest multiple

---
+++ SmallestMultiple/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SmallestMultiple
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int min = int.MaxValue;
+			for(int i = 1; min == int.MaxValue && i <= int.MaxValue; i++) {
+				for(int j = 2; j<= 20; j++) {
+					if(i % j != 0)
+						break;
+					if(j == 20)
+						min = i;
+				}
+			}
+			Console.WriteLine (min);
+		}
+	}
+}
