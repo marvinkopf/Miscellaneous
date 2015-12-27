2c8c3bcf83ae6f3f21b11341c47626c1ddec96fa
3rr0rl0l <marvinkopf@posteo.de>
2015-12-27 03:26:16 +0100

    add largest palindrome product

---
+++ LargestPalindromeProduct/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LargestPalindromeProduct
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int max = 0;
+			for(int i = 999; i > 0; i--) 
+				for(int j = i; j > 0; j--) {
+					if( IsPalindrome(i*j))
+						max = i*j > max ? i*j : max;
+				}
+
+			Console.WriteLine (max);
+		}
+
+		public static bool IsPalindrome(int inputNumber) {
+			string input = inputNumber.ToString();
+			string s = "";
+
+			for(int i = 0; i < input.Length; i++)
+				s = input[i] + s;
+			
+			if ( input == s)
+				return true;
+
+			return false; 
+		}
+	}
+}
