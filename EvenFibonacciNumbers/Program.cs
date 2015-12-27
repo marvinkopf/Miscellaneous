417fdc4e0c9b2b977efb4d2ac5519196a69c97d1
3rr0rl0l <marvinkopf@posteo.de>
2015-12-27 02:46:50 +0100

    add sum of even fibonacci numbers

---
+++ EvenFibonacciNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EvenFibonacciNumbers
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int result = 0;
+			for(int i = 2, j = 1, g = 1; i < 4000000; g = i, i += j, j = g) 
+				if(i % 2 == 0)
+					result += i;
+			Console.WriteLine (result);
+		}
+	}
+}
