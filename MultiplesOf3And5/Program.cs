c9c1ecbffa90b5972c6f6ffcb0b7077d334a2c8b
3rr0rl0l <marvinkopf@posteo.de>
2015-12-27 02:42:07 +0100

    add multiples of 3 and 5

---
+++ MultiplesOf3And5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MultiplesOf3And5
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{	
+			int result = 0;
+			for(int i = 0; i < 1000; i++)
+				if(i % 3 == 0 || i % 5 == 0)
+					result += i;
+			
+			Console.WriteLine (result);
+		}
+	}
+}
