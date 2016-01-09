a45e157d8bf80cacd79a90eb0df88823f6d0f3f2
3rr0rl0l <marvinkopf@posteo.de>
2016-01-09 20:32:51 +0100

    modulize and use ienumerable

--- EvenFibonacciNumbers/Program.cs
+++ EvenFibonacciNumbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EvenFibonacciNumbers
 {
@@ -7,10 +9,47 @@ namespace EvenFibonacciNumbers
 		public static void Main (string[] args)
 		{
 			int result = 0;
-			for(int i = 2, j = 1, g = 1; i < 4000000; g = i, i += j, j = g) 
-				if(i % 2 == 0)
-					result += i;
+			result = SumEvenFibonacciRecursive ();
 			Console.WriteLine (result);
+
+			result = SumEvenFibonacciIterative ();
+			Console.WriteLine (result);
+
+			// additionally: nth fibonacci number 
+			Console.WriteLine (FibonacciIterative ().ElementAt (40)); 
+			Console.WriteLine (FibonacciRecursive ().ElementAt (40));
+		}
+
+		public static int SumEvenFibonacciIterative ()
+		{
+			return SumEvenFibonacci (FibonacciIterative ());
+		}
+
+		public static int SumEvenFibonacciRecursive ()
+		{
+			return SumEvenFibonacci (FibonacciRecursive ());
+		}
+
+		public static int SumEvenFibonacci (IEnumerable<int> method)
+		{
+			return method.TakeWhile (x => x < 4000000).Where (x => x % 2 == 0).Sum ();
+		}
+
+		public static IEnumerable<int> FibonacciIterative ()
+		{
+			for (int i = 1, j = 0; i >= j; i += j, j = i - j)
+				yield return i; 
+		}
+
+		public static IEnumerable<int> FibonacciRecursive (int i = 1, int j = 0)
+		{
+			if (i < j)
+				yield break;
+			
+			yield return i;
+
+			foreach (int g in FibonacciRecursive(i+j,i))
+				yield return g;
 		}
 	}
 }
